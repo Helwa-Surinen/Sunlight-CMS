42b9c227745d0743c4742af3405c16be5849bc87
Helwa Surinen <153928113+Helwa-Surinen@users.noreply.github.com>
2025-08-04 10:47:28 +0200

    french iniciation

---
+++ french-language/plugins/languages/fr/class/FrenchLanguage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SunlightLanguage\Fr;
+
+use Sunlight\Plugin\LanguagePlugin;
+
+class FrenchLanguage extends LanguagePlugin
+{
+    function formatInteger(int $integer): string
+    {
+        if ($integer > 9999) {
+            return parent::formatInteger($integer);
+        }
+
+        return sprintf('%d', $integer);
+    }
+
+    function formatFloat(float $float, int $decimals): string
+    {
+        if ($float > 9999) {
+            return parent::formatFloat($float, $decimals);
+        }
+
+        return number_format($float, $decimals, $this->data->options['decimal_point'], '');
+    }
+}
---
+++ french-language/readme.rst
@@ -0,0 +1,22 @@
+French Language
+###################
+
+.. contents::
+
+Requirements
+************
+
+- Sunlight CMS 8
+
+Installation
+************
+
+::
+
+    Copy the folder 'plugins' and its contents to the root directory
+
+or
+
+::
+
+    Installation via administration: 'Administration > Plugins > Upload new plugins'
